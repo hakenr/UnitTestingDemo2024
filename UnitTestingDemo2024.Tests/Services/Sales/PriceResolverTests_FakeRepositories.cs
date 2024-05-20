b9f5d2f21ff1d745dc22e5433fb9824627ec7435
Robert Haken <haken@havit.cz>
2024-05-20 14:45:56 +0200

    Unit-test (fakes & mocks)

---
+++ UnitTestingDemo2024.Tests/Services/Sales/PriceResolverTests_FakeRepositories.cs
@@ -0,0 +1,121 @@
+﻿using UnitTestingDemo2024.Models.Sales;
+using UnitTestingDemo2024.Repositories.Sales;
+using UnitTestingDemo2024.Services.Sales;
+
+namespace UnitTestingDemo2024.Tests.Services.Sales;
+
+[TestClass]
+public class PriceResolverTests_FakeRepositories
+{
+	private const string CustomerWithIndividualPriceAndDiscount = "FAKE_CUST1";
+	private const string ProductGroupWithDiscount = "FAKE_PG1";
+	private const string ProductNumberWithBasicAndIndividualPrice = "FAKE_PN1";
+
+	[TestMethod]
+	public void PriceResolver_BasicPriceOnly_ReturnsBasicPrice()
+	{
+		// arrange
+		var resolver = new PriceResolver(
+			new FakeBasicPriceRepository(),
+			new FakeCustomerProductGroupDiscountRepository_NoDiscounts(),
+			new FakeCustomerPriceRepository_NoPrices());
+
+		// act
+		var resultPrice = resolver.GetPrice(
+			new Customer() { Name = "FAKE_OTHER_CUSTOMER" },
+			new Product()
+			{
+				ProductNumber = "FAKE_PN2",
+				ProductGroup = new ProductGroup()
+				{
+					Name = "FAKE_PGROUP"
+				}
+			});
+
+
+		// assert
+		Assert.AreEqual(200M, resultPrice);
+	}
+
+	[TestMethod]
+	public void PriceResolver_CustomerPriceAndProductGroupDiscount_ReturnsDiscountedCustomerPrice()
+	{
+		// arrange
+		var resolver = new PriceResolver(
+			new FakeBasicPriceRepository(),
+			new FakeCustomerProductGroupDiscountRepository(),
+			new FakeCustomerPriceRepository());
+
+		// act
+		var resultPrice = resolver.GetPrice(
+			new Customer() { Name = CustomerWithIndividualPriceAndDiscount },
+			new Product()
+			{
+				ProductNumber = ProductNumberWithBasicAndIndividualPrice,
+				ProductGroup = new ProductGroup()
+				{
+					Name = ProductGroupWithDiscount
+				}
+			});
+
+
+		// assert
+		Assert.AreEqual(81M, resultPrice);
+	}
+
+	public class FakeCustomerProductGroupDiscountRepository : ICustomerProductGroupDiscountRepository
+	{
+		public decimal? GetDiscountForCustomerAndProductGroup(Customer customer, ProductGroup productGroup)
+		{
+			if ((customer.Name == CustomerWithIndividualPriceAndDiscount) && (productGroup.Name == ProductGroupWithDiscount))
+			{
+				return 0.1M;
+			}
+			return null;
+		}
+	}
+
+	public class FakeCustomerProductGroupDiscountRepository_NoDiscounts : ICustomerProductGroupDiscountRepository
+	{
+		public decimal? GetDiscountForCustomerAndProductGroup(Customer customer, ProductGroup productGroup)
+		{
+			return null;
+		}
+	}
+
+	public class FakeBasicPriceRepository : IBasicPriceRepository
+	{
+		public decimal? GetBasicPriceForProduct(Product product)
+		{
+			switch (product.ProductNumber)
+			{
+				case ProductNumberWithBasicAndIndividualPrice:
+					return 100M;
+				case "FAKE_PN2":
+					return 200M;
+				default:
+					return null;
+			}
+		}
+	}
+
+	public class FakeCustomerPriceRepository_NoPrices : ICustomerPriceRepository
+	{
+		public decimal? GetCustomerPrice(Customer customer, Product product)
+		{
+			return null;
+		}
+	}
+
+	public class FakeCustomerPriceRepository : ICustomerPriceRepository
+	{
+		public decimal? GetCustomerPrice(Customer customer, Product product)
+		{
+			if ((customer.Name == CustomerWithIndividualPriceAndDiscount) && (product.ProductNumber == ProductNumberWithBasicAndIndividualPrice))
+			{
+				return 90M;
+			}
+			return null;
+		}
+	}
+}
---
+++ UnitTestingDemo2024.Tests/Services/Sales/PriceResolverTests_MoqRepositories.cs
@@ -0,0 +1,92 @@
+﻿using Moq;
+using UnitTestingDemo2024.Models.Sales;
+using UnitTestingDemo2024.Repositories.Sales;
+using UnitTestingDemo2024.Services.Sales;
+
+namespace UnitTestingDemo2024.Tests.Services.Sales;
+
+[TestClass]
+public class PriceResolverTests_MoqRepositories
+{
+	[TestMethod]
+	public void Moq_PriceResolver_BasicPriceOnly_ReturnsBasicPrice()
+	{
+		// arrange
+		const string productNumber = "FAKE_PN";
+
+		var basicPriceRepositoryMock = new Mock<IBasicPriceRepository>();
+		basicPriceRepositoryMock.Setup(r =>
+			r.GetBasicPriceForProduct(It.Is<Product>(product => product.ProductNumber == productNumber)))
+			.Returns(200M);
+
+		var resolver = new PriceResolver(
+			basicPriceRepositoryMock.Object,
+			new Mock<ICustomerProductGroupDiscountRepository>().Object,
+			new Mock<ICustomerPriceRepository>().Object);
+
+		// act
+		var resultPrice = resolver.GetPrice(
+			new Customer() { Name = "ANY_CUSTOMER" },
+			new Product()
+			{
+				ProductNumber = productNumber,
+				ProductGroup = new ProductGroup()
+				{
+					Name = "ANY_GROUP"
+				}
+			});
+
+
+		// assert
+		Assert.AreEqual(200M, resultPrice);
+	}
+
+	[TestMethod]
+	public void Moq_PriceResolver_CustomerPriceAndProductGroupDiscount_ReturnsDiscountedCustomerPrice()
+	{
+		// arrange
+		const string productNumber = "FAKE_PN";
+		const string customer = "FAKE_CUST";
+		const string productGroup = "FAKE_PG";
+
+		var basicPriceRepositoryMock = new Mock<IBasicPriceRepository>();
+		basicPriceRepositoryMock.Setup(r =>
+			r.GetBasicPriceForProduct(It.Is<Product>(product => product.ProductNumber == productNumber)))
+			.Returns(100M);
+
+		var customerProductGroupDicountRepositoryMock = new Mock<ICustomerProductGroupDiscountRepository>();
+		customerProductGroupDicountRepositoryMock.Setup(r =>
+			r.GetDiscountForCustomerAndProductGroup(
+				It.Is<Customer>(c => c.Name == customer),
+				It.Is<ProductGroup>(pg => pg.Name == productGroup)))
+			.Returns(0.1M);
+
+		var customerPriceRepositoryMock = new Mock<ICustomerPriceRepository>();
+		customerPriceRepositoryMock.Setup(r =>
+			r.GetCustomerPrice(
+				It.Is<Customer>(c => c.Name == customer),
+				It.Is<Product>(p => p.ProductNumber == productNumber)))
+			.Returns(90M);
+
+		var resolver = new PriceResolver(
+			basicPriceRepositoryMock.Object,
+			customerProductGroupDicountRepositoryMock.Object,
+			customerPriceRepositoryMock.Object);
+
+		// act
+		var resultPrice = resolver.GetPrice(
+			new Customer() { Name = customer },
+			new Product()
+			{
+				ProductNumber = productNumber,
+				ProductGroup = new ProductGroup()
+				{
+					Name = productGroup
+				}
+			});
+
+
+		// assert
+		Assert.AreEqual(81M, resultPrice);
+	}
+}
